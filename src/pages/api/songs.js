813381d6b5b0077554cc8076b3fb63fc1432bb2e
Chris Vogt <mail@chrisvogt.me>
2024-11-26 02:23:40 -0800

    Fill in a lot more song details

--- src/pages/api/songs.js
+++ src/pages/api/songs.js
@@ -1,4 +1,4 @@
-import songs from '@/data/repertoire-11252024.json';
+import songs from '@/data/repertoire-11262024.json';
 
 export default function handler(req, res) {
   res.status(200).json(songs);
