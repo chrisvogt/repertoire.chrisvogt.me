9d5079b87317ee2341ea9f8b6b6adae34cd3fd5a
Chris Vogt <mail@chrisvogt.me>
2024-12-07 10:02:05 -0800

    Sync with latest Google Sheet (12/7/24)

--- src/pages/api/songs.js
+++ src/pages/api/songs.js
@@ -1,4 +1,4 @@
-import songs from '@/data/repertoire-11272024.json';
+import songs from '@/data/repertoire-12072024.json';
 
 export default function handler(req, res) {
   res.status(200).json(songs);
