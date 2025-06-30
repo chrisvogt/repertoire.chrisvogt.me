55dd03d4a193eeab720b7686e8ed0e59944d4271
Chris Vogt <mail@chrisvogt.me>
2025-06-30 01:05:37 -0700

    chore: sync songs (June 30, 2025) (#3)
    This PR sync my website with
    https://docs.google.com/spreadsheets/d/1IRqgQCXxQ0KEqAdV119wN3FdhwVe_vtVjjPaowS5vFA/edit?gid=0#gid=0,
    which adds the following songs:
    
    • Twilight Zone - Ariana Grande
    • The Way We Were - Barbara Streisand
    • Lovelytheband - Broken
    • Touch - Daft Punk
    • Elliot James Reay - I Think They Call This Love
    • Golden Hour - JVKE
    • Chappell Roan - Pink Pony Club
    • Ashe & FINNEAS - Till Forever Falls Apart

--- src/pages/api/songs.js
+++ src/pages/api/songs.js
@@ -1,4 +1,4 @@
-import songs from '@/data/repertoire-01072025.json';
+import songs from '@/data/repertoire-06302025.json';
 
 export default function handler(req, res) {
   res.status(200).json(songs);
